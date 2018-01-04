70c85179696a044fcc1181099f6f761fb329df48
Amin Siddiqui <amin.s@codewalla.com>
2018-01-04 12:35:07 +0530

    - Implemented UI and related game playing logic

--- RockPaperScissor WatchKit Extension/InterfaceController.swift
+++ RockPaperScissor WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,37 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var imgVChallengeTool: WKInterfaceImage!
+    @IBOutlet var challengeOptions: WKInterfaceGroup!
+    
+    @IBOutlet var btnRock: WKInterfaceButton!
+    @IBOutlet var btnPaper: WKInterfaceButton!
+    @IBOutlet var btnScissor: WKInterfaceButton!
+    
+    @IBOutlet var lblWinLevel: WKInterfaceLabel!
+    
+    var challengeTool: RockPaperScissorGame.GameTool!
+    var level = 0
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        updateUI()
+        setChallengeTool()
+    }
+    
+    fileprivate func updateUI() {
+        btnRock.setBackgroundImage(UIImage(named: "rock"))
+        btnPaper.setBackgroundImage(UIImage(named: "paper"))
+        btnScissor.setBackgroundImage(UIImage(named: "scissor"))
+    }
+    
+    fileprivate func setChallengeTool() {
+        challengeTool = RockPaperScissorGame().selectRandomTool()
+        
+        let image = UIImage(named: "\(challengeTool!)")
+        imgVChallengeTool.setImage(image)
     }
     
     override func willActivate() {
@@ -27,5 +54,38 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    fileprivate func checkWin(_ isWin: Bool?) {
+        
+        if isWin == true {
+            level += 1
+        }
+        else if isWin == false {
+            level = max(level - 1, 0)
+        }
+        
+        lblWinLevel.setText("\(level)/20")
+        
+        setChallengeTool()
+    }
+    
+    @IBAction func btnRock_Action() {
+        let isWin = RockPaperScissorGame().toolIsBetter(RockPaperScissorGame.GameTool.rock,
+                                                        than: challengeTool)
+        checkWin(isWin)
+    }
+    
+    @IBAction func btnPaper_Action() {
+        let isWin = RockPaperScissorGame().toolIsBetter(RockPaperScissorGame.GameTool.paper,
+                                            than: challengeTool)
+        checkWin(isWin)
+    }
+    
+    @IBAction func btnScissor_Action() {
+        let isWin = RockPaperScissorGame().toolIsBetter(RockPaperScissorGame.GameTool.scissor,
+                                            than: challengeTool)
+        checkWin(isWin)
+    }
+    
 
 }
